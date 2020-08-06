61b91b4232d22a141c6ed28e1e5a8d89f20239f6
Elton Fonseca <elton869@gmail.com>
2020-08-06 16:54:13 -0300

    alteração widgets

---
+++ lib/app/core/view/navbar_padrao.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../tema/cores_const.dart';
+
+import 'tamanhos_relativos.dart';
+
+class NavbarPadrao extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      leading: Padding(
+        padding: EdgeInsets.only(left: displayWidth(context) * 0.07),
+        child: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      actions: [
+        Padding(
+            padding:
+                EdgeInsets.symmetric(horizontal: displayWidth(context) * 0.05),
+            child: IconButton(
+              icon: Icon(
+                Icons.menu,
+              ),
+              onPressed: () {
+                debugPrint('Menu Lateral');
+              },
+            )),
+      ],
+      backgroundColor: CoresConst.azulPadrao,
+    );
+  }
+}
