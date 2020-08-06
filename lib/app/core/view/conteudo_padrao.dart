68311c10af7b9ac1b53c88d6f51131d2dec297ff
elton fonseca <elton869@gmail.com>
2020-08-06 17:23:13 -0300

    add widget conteudo

---
+++ lib/app/core/view/conteudo_padrao.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'tamanhos_relativos.dart';
+
+class ConteudoPadrao extends StatelessWidget {
+
+  const ConteudoPadrao({this.textoCabecalho, this.conteudo,});
+
+  final Widget textoCabecalho;
+
+  final Widget conteudo;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+          child:
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start, 
+                children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: displayWidth(context) * 0.09,
+                      bottom: displayHeight(context) * 0.04,
+                      top: displayHeight(context) * 0.01),
+                  child: textoCabecalho,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(40.0),
+                          topRight: const Radius.circular(40.0),
+                        ),),
+                    width: displayWidth(context) * 1,
+                    child: conteudo
+                  ),
+                ),
+          ],
+          ),
+        );
+  }
+}
