9a304dd2a210e2ae0204ce617007c2c75111b498
Elton Fonseca <elton869@gmail.com>
2020-09-19 18:57:10 -0300

    apí publica

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -23,7 +23,7 @@ class AppModule extends MainModule {
         Bind<Dio>((i) {
           var dioClient = Dio();
 
-          dioClient.options.baseUrl = "http://192.168.15.12/api";
+          dioClient.options.baseUrl = "https://levaai-api.eltonfonseca.dev/api";
           dioClient.options.headers['content-Type'] = 'application/json';
           dioClient.options.headers["Api-token"] =
               '\$2a\$07\$usesomesillystringforeGsJAIIu7nhlxWq.cvdNluLcR1KdMYnq';
